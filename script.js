6d50b490e591f202c597bcafa9d42824283e3804
Kira <120910477+Krgnrlv@users.noreply.github.com>
2024-10-06 22:44:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let styleTheme = localStorage.getItem('styleTheme');
 const theme_btn = document.querySelector('.Theme_changer');
+const images = document.querySelectorAll('.slider_img')
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+let photoIndex = 0
 
 const enableDarkTheme = () =>{
     document.body.classList.add('dark_theme');
@@ -22,3 +26,24 @@ if (styleTheme === 'dark'){
 }else{
     disableDarkTheme();
 };
+
+function show (i){
+    images[photoIndex].classList.remove('active');
+    images[i].classList.add('active');
+    photoIndex = i
+}
+prev.addEventListener('click', () => {
+    let i = photoIndex - 1;
+    if (i < 0){
+        i=images.length - 1;
+    }
+    show(i)
+})
+next.addEventListener('click', () => {
+    let i = photoIndex + 1;
+    if (i >= images.length){
+        i = 0;
+    }
+    show(i)
+})
+show(photoIndex)
