3abbf8133b04549a7d0f10d471ad38dda1475996
Kira <120910477+Krgnrlv@users.noreply.github.com>
2024-10-07 23:53:31 +0100

    Add files via upload

---
+++ carousel.js
@@ -0,0 +1,25 @@
+const images = document.querySelectorAll('.slider_img');
+        prev = document.querySelector('.prev');
+        next = document.querySelector('.next');
+let photoIndex = 0;
+
+function show (i){
+    images[photoIndex].classList.remove('active');
+    images[i].classList.add('active');
+    photoIndex = i;
+}
+prev.addEventListener('click', () => {
+    let i = photoIndex - 1;
+    if (i < 0){
+        i=images.length - 1;
+    }
+    show(i)
+})
+next.addEventListener('click', () => {
+    let i = photoIndex + 1;
+    if (i >= images.length){
+        i = 0;
+    }
+    show(i);
+});
+show(photoIndex);
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let styleTheme = localStorage.getItem('styleTheme');
 const theme_btn = document.querySelector('.Theme_changer');
-const images = document.querySelectorAll('.slider_img')
-const prev = document.querySelector('.prev')
-const next = document.querySelector('.next')
-let photoIndex = 0
+
+const filltration = document.querySelector('.filltration')
+        Elements = document.querySelectorAll('.Element');
+        Filltartion_items = document.querySelectorAll('.filltration_item')
 
 const enableDarkTheme = () =>{
     document.body.classList.add('dark_theme');
@@ -27,23 +27,38 @@ if (styleTheme === 'dark'){
     disableDarkTheme();
 };
 
-function show (i){
-    images[photoIndex].classList.remove('active');
-    images[i].classList.add('active');
-    photoIndex = i
+
+function fillter(){
+    filltration.addEventListener('click', (event) =>{
+        const targetId = event.target.dataset.id
+        const target = event.target
+
+        if (target.classList.contains('filltration_item')){
+            Filltartion_items.forEach(Filltartion_items => Filltartion_items.classList.remove('filltration_active'))
+            target.classList.add('filltration_active')
+        }
+
+        switch(targetId){
+            case 'All':
+                getItems('Element')
+                break
+            case 'Homework':
+                getItems(targetId)
+                break
+            case 'Folk_project':
+                getItems(targetId)
+                break
+        }
+    });
+};
+fillter()
+
+function getItems(className){
+    Elements.forEach(item => {
+        if (item.classList.contains(className)){
+            item.style.display = 'block'
+        }else{
+            item.style.display = 'none'
+        }
+    });
 }
-prev.addEventListener('click', () => {
-    let i = photoIndex - 1;
-    if (i < 0){
-        i=images.length - 1;
-    }
-    show(i)
-})
-next.addEventListener('click', () => {
-    let i = photoIndex + 1;
-    if (i >= images.length){
-        i = 0;
-    }
-    show(i)
-})
-show(photoIndex)
